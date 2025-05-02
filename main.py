2efeacde832be5e1c033188781f5edc7f3856d3d
BPelletier258 <bernard_pelletier@videotron.ca>
2025-05-01 22:44:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -65,5 +65,6 @@ def analyse():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
